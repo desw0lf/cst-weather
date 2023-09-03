79aa2b428946d6d54141c94246cf159ab7c8dbdf
Artur <admin@deswolf.com>
2023-09-03 11:44:33 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
-import * as Realm from 'realm-web';
-import * as utils from './utils';
+import * as Realm from "realm-web";
+import * as utils from "./utils";
 
 // The Worker's environment bindings. See `wrangler.toml` file.
 interface Bindings {
     // MongoDB Realm Application ID
     REALM_APPID: string;
+    TOMORROW_API_KEY: string;
+    TOMORROW_BASE_URL: string;
 }
 
 // Define type alias; available via `realm-web`
 type Document = globalThis.Realm.Services.MongoDB.Document;
 
 // Declare the interface for a "todos" document
-interface Todo extends Document {
+interface Timelines extends Document {
     owner: string;
-    done: boolean;
-    todo: string;
+    queryId: string;
+    data: any;
+    startTime: any;
 }
 
 let App: Realm.App;
-const ObjectId = Realm.BSON.ObjectID;
+// const ObjectId = Realm.BSON.ObjectID;
+// const ISODate = Realm.
+
+const DB = "cst-weather";
+const COLLECTION = "timelines";
+
 
 // Define the Worker logic
 const worker: ExportedHandler<Bindings> = {
@@ -27,78 +35,107 @@ const worker: ExportedHandler<Bindings> = {
         App = App || new Realm.App(env.REALM_APPID);
 
         const method = req.method;
-        const path = url.pathname.replace(/[/]$/, '');
-        const todoID = url.searchParams.get('id') || '';
+        const pathname = url.pathname.replace(/[/]$/, '');
+        const prefix = "/api/weather";
+        const searchParams = url.searchParams;
+        searchParams.sort();
+        const queryId = searchParams.toString();
 
-        if (path !== '/api/todos') {
-            return utils.toError(`Unknown "${path}" URL; try "/api/todos" instead.`, 404);
+        if (!pathname.startsWith(prefix)) {
+            return utils.toError(`Unknown "${pathname}" URL;`, 404);
         }
 
         const token = req.headers.get('authorization');
-        if (!token) return utils.toError('Missing "authorization" header; try to add the header "authorization: REALM_API_KEY".', 401);
+        if (!token) return utils.toError('Unauthorized.', 401);
 
         try {
             const credentials = Realm.Credentials.apiKey(token);
             // Attempt to authenticate
             var user = await App.logIn(credentials);
-            var client = user.mongoClient('mongodb-atlas');
+            var client = user.mongoClient("mongodb-atlas");
         } catch (err) {
-            return utils.toError('Error with authentication.', 500);
+            return utils.toError("Error with authentication.", 500);
         }
 
         // Grab a reference to the "cloudflare.todos" collection
-        const collection = client.db('cloudflare').collection<Todo>('todos');
+        const collection = client.db(DB).collection<Timelines>(COLLECTION);
 
         try {
-            if (method === 'GET') {
-                if (todoID) {
+            if (method === "GET") {
+                if (queryId) {
                     // GET /api/todos?id=XXX
-                    return utils.reply(
-                        await collection.findOne({
-                            _id: new ObjectId(todoID)
-                        })
-                    );
+                    const found = await collection.findOne({
+                        queryId: queryId
+                    });
+                    if (found) {
+                        return utils.reply(found);
+                    }
+                    const remainingUrl = pathname.replace(new RegExp('^' + prefix), '');
+                    const targetUrl = decodeURIComponent(remainingUrl);
+                    function appendKey(sp: URLSearchParams) {
+                        sp.append("apikey", env.TOMORROW_API_KEY);
+                        return sp;
+                    }
+                    const tomorrowSearchParams = appendKey(searchParams).toString();
+                    const response = await fetch(env.TOMORROW_BASE_URL + targetUrl + "?" + tomorrowSearchParams, { headers: utils.BASE_HEADERS });
+                    const status = response.status;
+                    if (status !== 200) {
+                        return utils.toError(response.statusText, status);
+                    }
+                    const startTime = url.searchParams.get("startTime");
+                    const payload: { data: Timelines["data"] } = await response.json();
+                    const inserted = await collection.insertOne({
+                        startTime: { "$date" : startTime },
+                        owner: user.id,
+                        data: payload.data,
+                        queryId: queryId
+                    });
+                    if (inserted) {
+                        return utils.reply({ data: payload.data });
+                    }
+                    return utils.reply(inserted);
                 }
 
                 // GET /api/todos
-                return utils.reply(
-                    await collection.find()
-                );
+                // return utils.reply(
+                //     await collection.find()
+                // );
+                return utils.toError("Not found", 404);
             }
 
             // POST /api/todos
-            if (method === 'POST') {
-                const {todo} = await req.json();
-                return utils.reply(
-                    await collection.insertOne({
-                        owner: user.id,
-                        done: false,
-                        todo: todo,
-                    })
-                );
-            }
+            // if (method === 'POST') {
+            //     const {todo} = await req.json();
+            //     return utils.reply(
+            //         await collection.insertOne({
+            //             owner: user.id,
+            //             done: false,
+            //             todo: todo,
+            //         })
+            //     );
+            // }
 
             // PATCH /api/todos?id=XXX&done=true
-            if (method === 'PATCH') {
-                return utils.reply(
-                    await collection.updateOne({
-                        _id: new ObjectId(todoID)
-                    }, {
-                        $set: {
-                            done: url.searchParams.get('done') === 'true'
-                        }
-                    })
-                );
-            }
+            // if (method === 'PATCH') {
+            //     return utils.reply(
+            //         await collection.updateOne({
+            //             _id: new ObjectId(todoID)
+            //         }, {
+            //             $set: {
+            //                 done: url.searchParams.get('done') === 'true'
+            //             }
+            //         })
+            //     );
+            // }
 
             // DELETE /api/todos?id=XXX
-            if (method === 'DELETE') {
-                return utils.reply(
-                    await collection.deleteOne({
-                        _id: new ObjectId(todoID)
-                    })
-                );
-            }
+            // if (method === 'DELETE') {
+            //     return utils.reply(
+            //         await collection.deleteOne({
+            //             _id: new ObjectId(todoID)
+            //         })
+            //     );
+            // }
 
             // unknown method
             return utils.toError('Method not allowed.', 405);
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,20 @@
+export const BASE_HEADERS = {
+    "content-type": "application/json;charset=UTF-8"
+};
+
+const NO_CORS_HEADERS = {
+    ...BASE_HEADERS,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+    "Access-Control-Allow-Credentials": true.toString()
+};
+  
+
+
 export function toJSON(data: unknown, status = 200): Response {
     let body = JSON.stringify(data, null, 2);
-    let headers = {'content-type': 'application/json'};
+    let headers = NO_CORS_HEADERS;
     return new Response(body, {headers, status});
 }
 
